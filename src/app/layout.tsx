fab8378c556330f3dd90d0ecb90c173713b36f97
d25 <oladokundara@gmail.com>
2025-02-08 07:11:58 -0800

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter, Abril_Fatface } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-const abrilFatface = Abril_Fatface({ weight: '400', subsets: ['latin'] })
+// const abrilFatface = Abril_Fatface({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fashion',
